dfe984a19330d2226ce9260de3ffde2910790dd2
Nicholas Bailey <nickcyg1992@gmail.com>
2022-05-31 15:37:11 -0500

    create CERs and event listener

--- js/app.js
+++ js/app.js
@@ -1,1 +1,23 @@
-console.log('test')
+const todoInput = document.querySelector("#input");
+const btn = document.querySelector("#submit-button");
+const list = document.querySelector("#ul");
+
+let listDiv;
+
+init();
+
+
+
+btn.addEventListener('click', function(event) {
+  if(todoInput.value === "") return
+  const newItem = document.createElement('li')
+  newTodo.textContent = todoInput.value
+  list.appendChild(newItem)
+  todoInput.value = ''
+})
+
+
+
+function init() {
+  listDiv = [];
+}
